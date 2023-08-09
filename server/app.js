ce924646287d5e7ab6a73847f69a0df8ff713191
Ashikur Rahman <ashikmoinkhan@gmail.com>
2023-08-09 07:56:36 +0600

    Created Conversation Api

--- server/app.js
+++ server/app.js
@@ -2,6 +2,11 @@ const express = require('express');
 const bcryptjs = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const cors = require('cors');
+const io = require('socket.io')(8080, {
+    cors: {
+        origin: 'http://localhost:3000',
+    }
+});
 
 
 // Connect to DB
@@ -22,6 +27,55 @@ app.use(cors());
 
 
 const port = process.env.PORT || 5000;
+
+
+
+// Socket.io
+let users = [];
+io.on('connection', socket => {
+    console.log('User connected', socket.id);
+    socket.on('addUser', userId => {
+        const isUserExist = users.find(user => user.userId === userId);
+        if (!isUserExist) {
+            const user = { userId, socketId: socket.id };
+            users.push(user);
+            io.emit('getUsers', users);
+        }
+    });
+
+    socket.on('sendMessage', async ({ senderId, receiverId, message, conversationId }) => {
+        const receiver = users.find(user => user.userId === receiverId);
+        const sender = users.find(user => user.userId === senderId);
+        const user = await Users.findById(senderId);
+        console.log('sender :>> ', sender, receiver);
+        if (receiver) {
+            io.to(receiver.socketId).to(sender.socketId).emit('getMessage', {
+                senderId,
+                message,
+                conversationId,
+                receiverId,
+                user: { id: user._id, fullName: user.fullName, email: user.email }
+            });
+            }else {
+                io.to(sender.socketId).emit('getMessage', {
+                    senderId,
+                    message,
+                    conversationId,
+                    receiverId,
+                    user: { id: user._id, fullName: user.fullName, email: user.email }
+                });
+            }
+        });
+
+    socket.on('disconnect', () => {
+        users = users.filter(user => user.socketId !== socket.id);
+        io.emit('getUsers', users);
+    });
+    // io.emit('getUsers', socket.userId);
+});
+
+
+
 
 // All Routes
 app.get('/', (req, res) =>{
